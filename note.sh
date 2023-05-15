bf44e79d240896d5bb580d25221377b85b0ded78
Krzysztof Kucharczyk <toficzak@gmail.com>
2023-05-15 16:03:13 +0200

    Minor upgrades

--- note.sh
+++ note.sh
@@ -5,11 +5,7 @@
 set -e
 
 _CURRENT_PATH=$(pwd)
-_DAYS_FOLDER_NAME=${DAYS_FOLDER_NAME} # default
-if [[ -z ${DAYS_FOLDER_NAME} ]]; then
-	_DAYS_FOLDER_NAME=days
-fi
-
+_DAYS_FOLDER_NAME="${DAYS_FOLDER_NAME:-days}"
 _DAYS_FOLDER_PATH="${_CURRENT_PATH}/${_DAYS_FOLDER_NAME}"
 
 if [[ ! -d ${_DAYS_FOLDER_PATH} ]]; then
@@ -23,29 +19,22 @@ fi
 _CURRENT_DATE=$(date)
 _CURRENT_DATE_FORMATTED=$(date +%Y%m%d)
 _DAY_FILE_PATH="${_DAYS_FOLDER_PATH}/${_CURRENT_DATE_FORMATTED}"
-_TEMPLATE_DAY_FILE_NAME=${TEMPLATE_DAY_FILE_NAME}
-
-if [[ -z ${TEMPLATE_DAY_FILE_NAME} ]]; then
-	_TEMPLATE_DAY_FILE_NAME=".template_day"
-fi
-
+_TEMPLATE_DAY_FILE_NAME="${TEMPLATE_DAY_FILE_NAME:-.template_day}"
 _TEMPLATE_DAY_FILE_PATH="${_CURRENT_PATH}/${_TEMPLATE_DAY_FILE_NAME}"
 
 if [[ ! -f ${_DAY_FILE_PATH} ]]; then
 	echo "Creating file for day: ${_CURRENT_DATE}"
 	if [[ -f ${_TEMPLATE_DAY_FILE_PATH} ]]; then
 		cp "${_TEMPLATE_DAY_FILE_PATH}" "${_DAY_FILE_PATH}"
+		# replace date in template
+    sed -i "s/today_date/$(date +%d-%m-%Y)/g" "${_DAY_FILE_PATH}"
 	else
 		touch "${_DAY_FILE_PATH}"
 	fi
 	echo "Created file: ${_DAY_FILE_PATH} ✅"
 fi
 
-_NOTE_LINK_NAME=${NOTE_LINK_NAME}
-if [[ -z ${NOTE_LINK_NAME} ]]; then
-	_NOTE_LINK_NAME="note"
-fi
-
+_NOTE_LINK_NAME="${NOTE_LINK_NAME:-note}"
 _CURRENT_NOTE_LINK="${HOME}/${_NOTE_LINK_NAME}"
 
 if [[ -f ${_CURRENT_NOTE_LINK} ]]; then
@@ -58,13 +47,10 @@ echo "Set properly: "
 ls -l "${HOME}" | grep -w "${_NOTE_LINK_NAME}"
 echo ""
 
-_ADDITIONAL_SCRIPT_PATH=${ADDITIONAL_SCRIPT_PATH}
-if [[ -z ${ADDITIONAL_SCRIPT_PATH} ]]; then
-	_ADDITIONAL_SCRIPT_PATH="${_CURRENT_PATH}/additional_commands.sh"
-fi
-
+_ADDITIONAL_SCRIPT_PATH="${ADDITIONAL_SCRIPT_PATH:-${_CURRENT_PATH}/additional_commands.sh}"
 if [[ -f ${_ADDITIONAL_SCRIPT_PATH} ]]; then
 	echo "Performing additional operations from: ${_ADDITIONAL_SCRIPT_PATH}"
 	. ${_ADDITIONAL_SCRIPT_PATH}
 fi
 
+echo "Done 😺"
