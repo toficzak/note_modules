146e82aeb7016d41a7c96f42b3539fc4f7d50b95
Krzysztof Kucharczyk <toficzak@gmail.com>
2023-05-15 16:15:26 +0200

    Fix desc and var names

--- note.sh
+++ note.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 # Author: Toficzak
-# Desc: create directory structure to keep notes, adds new file per day if it does not exists, sets properly shortcut to current day and performs additional operations if requested (these are considered as environment sensitive, will not be stored in vcs)
+# Desc: create directory structure to keep notes, adds new file per day if it does not exists, sets properly shortcut
+# to current day and performs additional operations if requested (these are considered as environment sensitive,
+# will not be stored in vcs).
+#
+# Configurable (thorough envs):
+# - NM_DAYS_FOLDER_NAME - name of folder which keeps files per days
+# - NM_TEMPLATE_DAY_FILE_NAME - template to be used as a base day
+# - NM_NOTE_LINK_NAME - name of the link in home folder
+# - NM_ADDITIONAL_SCRIPT_PATH - path to script which should be performed after day note creation
+#
+# Variables stating with '_' are local and should not be published.
 
 set -e
 
 _CURRENT_PATH=$(pwd)
-_DAYS_FOLDER_NAME="${DAYS_FOLDER_NAME:-days}"
+_DAYS_FOLDER_NAME="${NM_DAYS_FOLDER_NAME:-days}"
 _DAYS_FOLDER_PATH="${_CURRENT_PATH}/${_DAYS_FOLDER_NAME}"
 
 if [[ ! -d ${_DAYS_FOLDER_PATH} ]]; then
@@ -19,7 +29,7 @@ fi
 _CURRENT_DATE=$(date)
 _CURRENT_DATE_FORMATTED=$(date +%Y%m%d)
 _DAY_FILE_PATH="${_DAYS_FOLDER_PATH}/${_CURRENT_DATE_FORMATTED}"
-_TEMPLATE_DAY_FILE_NAME="${TEMPLATE_DAY_FILE_NAME:-.template_day}"
+_TEMPLATE_DAY_FILE_NAME="${NM_TEMPLATE_DAY_FILE_NAME:-.template_day}"
 _TEMPLATE_DAY_FILE_PATH="${_CURRENT_PATH}/${_TEMPLATE_DAY_FILE_NAME}"
 
 if [[ ! -f ${_DAY_FILE_PATH} ]]; then
@@ -34,7 +44,7 @@ if [[ ! -f ${_DAY_FILE_PATH} ]]; then
 	echo "Created file: ${_DAY_FILE_PATH} ✅"
 fi
 
-_NOTE_LINK_NAME="${NOTE_LINK_NAME:-note}"
+_NOTE_LINK_NAME="${NM_NOTE_LINK_NAME:-note}"
 _CURRENT_NOTE_LINK="${HOME}/${_NOTE_LINK_NAME}"
 
 _CURRENT_LINK_POINT_TO=$(readlink -f "${_CURRENT_NOTE_LINK}" | xargs basename)
@@ -50,7 +60,7 @@ if [[ "${_CURRENT_DATE_FORMATTED}" != "${_CURRENT_LINK_POINT_TO}" ]]; then
   echo ""
 fi
 
-_ADDITIONAL_SCRIPT_PATH="${ADDITIONAL_SCRIPT_PATH:-${_CURRENT_PATH}/additional_commands.sh}"
+_ADDITIONAL_SCRIPT_PATH="${NM_ADDITIONAL_SCRIPT_PATH:-${_CURRENT_PATH}/additional_commands.sh}"
 if [[ -f ${_ADDITIONAL_SCRIPT_PATH} ]]; then
 	echo "Performing additional operations from: ${_ADDITIONAL_SCRIPT_PATH}"
 	. ${_ADDITIONAL_SCRIPT_PATH}
